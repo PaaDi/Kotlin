85423d70ba6f6e09f026b903f6d0247bf43a541c
MAXIME PALETOU <maxime.paletou@icloud.com>
2021-03-11 16:25:24 +0100

    Contact interface implements 2 - Bugs fixes

---
+++ app/src/main/java/com/madera/kotlin/Repository/ContactRepository.kt
@@ -0,0 +1,14 @@
+package com.madera.kotlin.Repository
+
+import androidx.annotation.WorkerThread
+import com.madera.kotlin.Dao.ContactDao
+import com.madera.kotlin.Entity.Contact
+
+class ContactRepository(private val contactDao: ContactDao) {
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun createContact(contact: Contact){
+        contactDao.createContact(contact)
+    }
+}
---
+++ app/src/main/java/com/madera/kotlin/ViewModel/ContactViewModel.kt
@@ -0,0 +1,24 @@
+package com.madera.kotlin.ViewModel
+
+import androidx.lifecycle.*
+import com.madera.kotlin.Entity.Contact
+import com.madera.kotlin.Repository.ContactRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class ContactViewModel(private val repository: ContactRepository) : ViewModel() {
+
+    fun createContact(contact: Contact) = viewModelScope.launch {
+        repository.createContact(contact)
+    }
+}
+
+class ContactViewModelFactory(private val repository: ContactRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T{
+        if (modelClass.isAssignableFrom(ContactViewModel::class.java)){
+            @Suppress("UNCHECKED_CAS")
+            return ContactViewModel(repository) as T
+        }
+        throw IllegalArgumentException("ViewModel Inconnu (Contact)")
+    }
+}
