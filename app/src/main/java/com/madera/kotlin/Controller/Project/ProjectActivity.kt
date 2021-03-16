c73fced1fd1a743e7b903d6b67442192a5ae7ebd
Melvin <68494721+PaaDi@users.noreply.github.com>
2021-03-16 11:38:44 +0100

    Espace Projet - Nav

--- app/src/main/java/com/madera/kotlin/Controller/Project/ProjectActivity.kt
+++ app/src/main/java/com/madera/kotlin/Controller/Project/ProjectActivity.kt
@@ -11,27 +11,10 @@ import com.madera.kotlin.R
 
 class ProjectActivity : AppCompatActivity() {
 
-    var btn_popup: Button? = null
-
-    lateinit var title: Array<String>
-    var spinner_item: String? = null
-
-    var adapter: SpinnerAdapter? = null
-
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.project_view)
-
-        val btnFilter = findViewById<Spinner>(R.id.spinner1)
-        val items = arrayOf("François", "Yves", "Guillaume", "Marta", "Mélanie")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
-        //set the spinners adapter to the previously created one.
-        btnFilter.adapter = adapter
-
-
-
-
 
     }
 
