eabcce031cc08026a42539caa62310c9e26c9079
Melvin <68494721+PaaDi@users.noreply.github.com>
2021-03-24 14:56:36 +0100

    ajout icon recycler contact + taille et police bouton menu detail client

--- app/src/main/java/com/madera/kotlin/Controller/Contact/ContactListAdapter.kt
+++ app/src/main/java/com/madera/kotlin/Controller/Contact/ContactListAdapter.kt
@@ -36,6 +36,7 @@ class ContactListAdapter(private val contactViewModel: ContactViewModel, context
         private val buttonSuppressContact: Button = itemView.findViewById(R.id.btn_deleteContact)
         val phoneTextView: ImageView = itemView.findViewById(R.id.iconPhone)
         val mailTextView: ImageView = itemView.findViewById(R.id.iconEmail)
+        val userTextView: ImageView = itemView.findViewById(R.id.iconUser)
 
 
         fun bind(text: String?, text2: String?, text3: String?, contact: Contact, viewModel: ContactViewModel, API : MaderaAPI){
@@ -44,6 +45,7 @@ class ContactListAdapter(private val contactViewModel: ContactViewModel, context
             contactItemView3.text = text3
             phoneTextView.setImageResource(R.drawable.ic_phone)
             mailTextView.setImageResource(R.drawable.ic_email)
+            userTextView.setImageResource(R.drawable.ic_user)
             buttonSuppressContact.setOnClickListener {
                 viewModel.deleteContact(contact)
                 API.suppressionContactAPI(contact.refContact.toString())
